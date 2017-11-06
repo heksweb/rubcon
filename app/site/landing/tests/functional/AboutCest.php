d5dca02efe59cb0c2b13f56c5f70b420117dd92e
Unknown <heksweb@gmail.com>
2017-11-06 18:09:08 +0300

    adding landing folder

---
+++ app/site/landing/tests/functional/AboutCest.php
@@ -0,0 +1,13 @@
+<?php
+namespace landing\tests\functional;
+
+use landing\tests\FunctionalTester;
+
+class AboutCest
+{
+    public function checkAbout(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/about');
+        $I->see('About', 'h1');
+    }
+}
