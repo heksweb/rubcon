2192735d45dc5207efbe1f4ff49951ff93bb64e0
Unknown <heksweb@gmail.com>
2017-11-06 19:18:23 +0300

    fix some components with alias

--- app/site/common/config/bootstrap.php
+++ app/site/common/config/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@landing', dirname(dirname(__DIR__)) . '/landing');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
