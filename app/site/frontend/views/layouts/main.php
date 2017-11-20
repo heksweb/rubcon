4f9f6a854e27915dd276292fd075779254a4ef29
Unknown <heksweb@gmail.com>
2017-11-20 13:57:05 +0300

    Добвлены пункты меню

--- app/site/frontend/views/layouts/main.php
+++ app/site/frontend/views/layouts/main.php
@@ -29,33 +29,41 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => '<span class="glyphicon glyphicon-home"></span>',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        // ['label' => '<span class="glyphicon glyphicon-home"></span>', 'url' => ['/site/index']],
+        ['label' => 'О Компании', 'url' => ['/site/about']],
+        ['label' => 'Услуги', 'url' => ['/site/contact']],
+        ['label' => 'Расчет стоимости', 'url' => ['/site/contact']],
+        ['label' => 'Отзывы', 'url' => ['/site/contact']],
+        ['label' => 'Оборудование', 'url' => ['/site/contact']],
+        ['label' => 'Прайс-лист', 'url' => ['/site/contact']],
+        ['label' => 'Абонентам', 'url' => ['/site/contact']],
+        ['label' => 'Контакты', 'url' => ['/site/contact']],
+        ['label' => 'Консультация', 'url' => ['/site/contact']],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
+    // if (Yii::$app->user->isGuest) {
+    //     $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+    //     $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    // } else {
+    //     $menuItems[] = '<li>'
+    //         . Html::beginForm(['/site/logout'], 'post')
+    //         . Html::submitButton(
+    //             'Logout (' . Yii::$app->user->identity->username . ')',
+    //             ['class' => 'btn btn-link logout']
+    //         )
+    //         . Html::endForm()
+    //         . '</li>';
+    // }
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav navbar-left'],
         'items' => $menuItems,
+        'encodeLabels' => false,
     ]);
     NavBar::end();
     ?>
